bcde0c90e23f9c1f8f5c07a9f2fc77e914fba297
KNOCKSSTUDiOS <knocksstudios@gmail.com>
2026-07-19 04:57:09 -0700

    Add first workflow

---
+++ build/CMakeFiles/bagheera_engine.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bagheera_engine.dir/link.d"
+  "CMakeFiles/bagheera_engine.dir/src/config_parser.cpp.o"
+  "CMakeFiles/bagheera_engine.dir/src/config_parser.cpp.o.d"
+  "CMakeFiles/bagheera_engine.dir/src/main.cpp.o"
+  "CMakeFiles/bagheera_engine.dir/src/main.cpp.o.d"
+  "CMakeFiles/bagheera_engine.dir/src/platform_bridge.cpp.o"
+  "CMakeFiles/bagheera_engine.dir/src/platform_bridge.cpp.o.d"
+  "CMakeFiles/bagheera_engine.dir/src/system_diag.cpp.o"
+  "CMakeFiles/bagheera_engine.dir/src/system_diag.cpp.o.d"
+  "CMakeFiles/bagheera_engine.dir/src/system_io.cpp.o"
+  "CMakeFiles/bagheera_engine.dir/src/system_io.cpp.o.d"
+  "bagheera_engine"
+  "bagheera_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bagheera_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
